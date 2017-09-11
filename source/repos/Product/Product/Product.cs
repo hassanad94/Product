6046a823b7d53936e00c7e3f92f7bcd32d463591
unknown <bunyikflorian@terem307pc5.athenepraxis.local>
2017-09-11 18:32:26 +0200

    Product class properties

--- source/repos/Product/Product/Product.cs
+++ source/repos/Product/Product/Product.cs
@@ -10,8 +10,34 @@ namespace Product
     class Product : BaseModel
     {
         string _name;
-        int _value;
+        int _price;
         int _quantity;
+
+
+        public string Name {
+
+            get { return _name; }
+            set {
+                _name = value;
+                OnPropertyChange();
+            }
+        }
+
+        public int Price
+        {
+            get { return _price; }
+            set {
+                _price = value;
+                    OnPropertyChange();
+            }
+        }
+
+        public int Quantity {
+            get {return _quantity }
+            set { _quantity = value;
+                OnPropertyChange();
+            }
+        }
 
     }
 }
